2e38a19a07e00797658e262fd1bf4d8800cb68f5
augustolorenzetti <lorenzettiaugusto25@gmail.com>
2025-01-15 18:53:03 +0100

    added arithmetic and boolean

---
+++ Arithmetic_file.py
@@ -0,0 +1,14 @@
+print(2+6)
+print(9-99)
+print(20/5)
+print(22/5)
+print(20/5) # always produces float result
+print(20/5) # this is the integer division!
+print(22%5)
+print(10%6)
+print(2**3)
+print(2*3**6)
+
+
+
+
---
+++ bool_operator.py
@@ -0,0 +1,10 @@
+print(not True)
+print(not False)
+print (not 7) #false because 7 is True
+print(True or False)
+print(2 or 3 or 4)
+print(False or 0 or 4 or True)
+print(1 and 2 and 3)
+print(1 and 0.0 and 4)
+print(False or 0 or None)
+
